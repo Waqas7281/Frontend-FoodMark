e0b56299b683818359f64c8359d20b09952b4088
Waqas7281 <you@example.com>
2025-10-13 14:19:17 +0500

    production ready

--- src/pages/forgotPassword.jsx
+++ src/pages/forgotPassword.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { IoIosArrowRoundBack } from "react-icons/io";
 import axios from "axios"
-import server from '../App'
+import { server } from '../App'
 
 function ForgotPassword() {
   const navigate = useNavigate();
